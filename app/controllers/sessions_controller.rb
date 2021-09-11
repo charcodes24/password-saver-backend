f94107a678703d09ccc1eeffe2dc623389bd5597
charcodes24 <charcodes24@gmail.com>
2021-09-11 12:46:54 -0700

    Fixed typo in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
 
     def create
-        user = User.find_by(user_name: params[:username])
+        user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id 
+            byebug
             render json: user, status: :created
         else 
             render json: { errors: ["Invalid username or password"] }, status: :unauthorized
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # resources :passwords
   resources :users, only: [:create]
-  resources :sessions, only: [:create]
+
+  #user login
+  post "/login", to: "sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
