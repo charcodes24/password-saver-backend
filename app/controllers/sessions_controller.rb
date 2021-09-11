0a8443a1cdf0d4e267e1b2c88cf8596b415957d0
charcodes24 <charcodes24@gmail.com>
2021-09-11 13:13:45 -0700

    Added session#destroy and route logout to handle user logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,15 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:username])
         if user&.authenticate(params[:password])
             session[:user_id] = user.id 
-            byebug
+            # byebug
             render json: user, status: :created
         else 
             render json: { errors: ["Invalid username or password"] }, status: :unauthorized
         end
+    end
+
+    def destroy 
+        session.destroy 
+        render json: { message: "user logged out"}
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   #user login
   post "/login", to: "sessions#create"
+  #user logout
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
