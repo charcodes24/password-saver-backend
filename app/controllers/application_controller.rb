733cdf5124f4c2839b277fdbe09b137974074a4c
charcodes24 <charcodes24@gmail.com>
2021-09-11 12:31:46 -0700

    Added sessions#create and applications#authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,11 @@ class ApplicationController < ActionController::API
   def render_unprocessable_entity_response(e)
     render json: { errors: [e.record.errors.full_messages] }, status: :unprocessable_entity
   end
+
+  def authorize
+    unless session.include?(:user_id)
+      render json: { errors: ['Unauthorized access, please login'] },
+             status: :unauthorized
+    end
+  end
 end
