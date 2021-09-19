b6fb01736598407275e3912d18ff0182d874d33f
charcodes24 <charcodes24@gmail.com>
2021-09-18 17:23:52 -0700

    Fixed error rendering on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found_response
 
 
     private 
@@ -10,11 +11,15 @@ class ApplicationController < ActionController::API
         render json: { errors: ["All fields must be filled out"] }, status: :unprocessable_entity
     end
 
+    def render_record_not_found_response
+        render json: { errors: ["Please enter a valid username & password."] }, status: :not_found
+    end
+
     
 
     def authorize
         unless session.include?(:user_id)
-        render json: { errors: ['Unauthorized access, please login'] },
+        render json: { errors: ['Please login to proceed to your page...'] },
                 status: :unauthorized
         end
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
     before_action :authorize, only: [:destroy]
 
     def create
-        user = User.find_by(username: params[:username])
+        user = User.find_by!(username: params[:username])
+        byebug
         if user&.authenticate(params[:password])
             session[:user_id] = user.id 
             render json: user, status: :created
